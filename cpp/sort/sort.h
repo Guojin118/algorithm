a1fb2614cbfbb346c093f2d77c8eedc5b6ea2ee0
guojin.wei <guojin.wei.118@gmail.com>
2020-01-28 11:02:04 +0800

    support  .h files and add sort.h

---
+++ cpp/sort/sort.h
@@ -0,0 +1,14 @@
+#ifndef __SORT_H_
+#define __SORT_H_
+
+template <typename T>
+extern void quicksort(T A[], int len);
+
+template <typename T>
+void mergesort(T A[], int len);
+
+template <typename T>
+void heapsort(T A[], int len);
+
+
+#endif
